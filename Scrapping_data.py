64974c26717b37056c7830faa9d5738ab617b8d8
Vedant Dobhal <vedantdobhalgermany@gmail.com>
2026-04-21 21:52:45 +0200

    added next button functionality

--- Scrapping_data.py
+++ Scrapping_data.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 
-
 driver = webdriver.Chrome()
 driver.maximize_window()
 
@@ -25,17 +24,28 @@ def wait_for_page_to_load(driver,wait):
     )
 
     except:
-        print(f"The page \"{page_title}\" did not get fully loaded within the given duration.")
+        print(f"The page \"{page_title}\" did not get fully loaded within the given duration.\n")
     
     else:
-        print(f"The page \"{page_title}\" is successfully loaded!")
-        # Declining cookies
-        wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button[2]'))).click()
+        print(f"The page \"{page_title}\" is successfully loaded!\n")
+        
 
 
 url = "https://finance.yahoo.com/"
 driver.get(url)
 wait_for_page_to_load(driver,wait)
+
+# Declining cookies
+try:
+    cookie_btn = wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button[2]')
+        )
+    )
+    cookie_btn.click()
+
+except:
+    print("Cookie popup not found.")
 
 actions = ActionChains(driver)
 
@@ -55,10 +65,30 @@ actions.move_to_element(trending_tickers).perform()
 trending_tickers_click = wait.until(EC.element_to_be_clickable((By.XPATH, '/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/ol[1]/li[3]/ol[1]/li[1]/ol[1]/li[4]/a[1]')))
 trending_tickers_click.click()
 
+wait_for_page_to_load(driver,wait)
+
 # Click on Most Active
 most_active = wait.until(EC.element_to_be_clickable((By.XPATH,'/html[1]/body[1]/div[1]/div[4]/main[1]/section[1]/section[1]/section[1]/section[1]/section[1]/div[1]/div[1]/div[1]/a[1]')))
 most_active.click()
 wait_for_page_to_load(driver,wait)
+
+# scraping the data
+while True:
+    # scraping
+    
+
+    # click next
+    try:
+        next_button = wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div[4]/main/section/section/section/section/section[1]/div/div[4]/div[3]/button[3]/div/svg')))
+    except:
+        print("The \"next button\" is not clickable. We have navigated through all the pages.")
+        break
+    else:
+        next_button.click()
+        time.sleep(1)
+
+
+
 
 
 time.sleep(2)
